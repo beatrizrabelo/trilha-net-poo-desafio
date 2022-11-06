42dfcbc7db3ba7958f450ea647a9abd014d0c091
Beatriz Rabelo <beatriz.devrabelo@gmail.com>
2022-11-06 15:50:52 -0300

    first commit

---
+++ Gerenciador.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioSistemaCelular
+{
+    public class Gerenciador
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
